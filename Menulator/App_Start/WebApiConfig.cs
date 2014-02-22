be563c619b2228a265dee72834fe5e3117de8006
ShawLevin <shawlevin@gmail.com>
2014-02-22 12:20:21 -0500

    EntityFramework and the restaurant controller.

--- Menulator/App_Start/WebApiConfig.cs
+++ Menulator/App_Start/WebApiConfig.cs
@@ -25,6 +25,9 @@ namespace Menulator
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+
+            var appXmlType = config.Formatters.XmlFormatter.SupportedMediaTypes.FirstOrDefault(t => t.MediaType == "application/xml");
+            config.Formatters.XmlFormatter.SupportedMediaTypes.Remove(appXmlType);
         }
     }
 }
